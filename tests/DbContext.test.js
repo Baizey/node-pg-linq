16f7a4565757c9d48cf7c3e41076e590d06869bb
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2019-11-30 03:53:25 +0100

    move tests

--- tests/DbContext.test.js
+++ tests/DbContext.test.js
@@ -2,8 +2,57 @@
 import should from "should";
 import 'regenerator-runtime';
 import DbContext from "../src";
+const {Pool, Client} = require('pg');
+
+/**
+ * @param {DbContext} context
+ * @returns {Promise<void>}
+ */
+const createEmptyTable = async (context) => {
+    const create = context.create().ignoreIfExists();
+    create.serial('id').primary();
+    create.text('name').nullable(false);
+    await create.run();
+    await context.delete().run();
+};
+/**
+ * @param {DbContext} context
+ * @returns {PG.Client}
+ */
+const addClient = context => {
+    const client = new Client();
+    client.connect();
+    context.usingClient(client);
+    return client;
+};
+
+/**
+ * @param {DbContext} context
+ * @returns {PG.Pool}
+ */
+const addPool = context => {
+    const pool = new Pool();
+    context.usingPool(pool);
+    return pool;
+};
 
 describe("DbContext", () => {
+    it('connect and use client', async () => {
+        const name = 'pg-linq-test-table-client';
+        const context = new DbContext(name);
+        const conn = addClient(context);
+        await createEmptyTable(context);
+        conn.end();
+    });
+
+    it('connect and use pool', async () => {
+        const name = 'pg-linq-test-table-pool';
+        const context = new DbContext(name);
+        const conn = addPool(context);
+        await createEmptyTable(context);
+        conn.end();
+    });
+
     it('get select query', () => {
         const context = new DbContext('table');
         context.select().toString().should.equal('SELECT * FROM table');
